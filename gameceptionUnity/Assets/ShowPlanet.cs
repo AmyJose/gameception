ff064e275422c7af452e52f5ec1dd5bb7b475ded
kaungzhan <kaungzawhan2000@gmail.com>
2026-01-23 14:32:07 +0000

    planet-appears-when-p-pressed

---
+++ gameceptionUnity/Assets/ShowPlanet.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShowPlanet : MonoBehaviour
+{
+    public GameObject planet;
+
+    void Start()
+    {
+        planet.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.pKey.wasPressedThisFrame)
+        {
+            planet.SetActive(true);
+        }
+    }
+}
