a4f25c0dc0bd40b5a375175b03832f1faf40c465
elsawakfi <xp23980@bristol.ac.uk>
2026-01-26 19:47:04 +0000

    When p is pressed it is duplicating up to 4 planets

--- gameceptionUnity/Assets/ShowPlanet.cs
+++ gameceptionUnity/Assets/ShowPlanet.cs
@@ -4,6 +4,9 @@ using UnityEngine.InputSystem;
 public class ShowPlanet : MonoBehaviour
 {
     public GameObject planet;
+    public Transform parent;
+    public float spacing = 2.0f;
+    private int count;
 
     void Start()
     {
@@ -12,9 +15,16 @@ public class ShowPlanet : MonoBehaviour
 
     void Update()
     {
-        if (Keyboard.current != null && Keyboard.current.pKey.wasPressedThisFrame)
-        {
+        if (Keyboard.current != null && Keyboard.current.pKey.wasPressedThisFrame && count < 8)
+        {   
+            var p = parent != null ? parent : planet.transform.parent;
+            GameObject clone = Instantiate(planet, p);
+            clone.name = $"{planet.name}_Clone_{count++}";
+            clone.transform.position = planet.transform.position + Vector3.right * spacing * count;
+            clone.transform.rotation = planet.transform.rotation;
+            clone.transform.localScale = planet.transform.localScale;
             planet.SetActive(true);
+            count++;
         }
     }
 }
