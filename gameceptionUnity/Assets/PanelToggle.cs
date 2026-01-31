569bdb63c4c735ab6756bd8c280d249dfd1849cd
elsawakfi <xp23980@bristol.ac.uk>
2026-01-31 18:20:49 +0000

    Added two more panels for the tutorial

--- gameceptionUnity/Assets/PanelToggle.cs
+++ gameceptionUnity/Assets/PanelToggle.cs
@@ -4,6 +4,8 @@ public class PanelToggle : MonoBehaviour
 {
     public GameObject firstPanel; 
     public GameObject secondPanel; 
+    public GameObject thirdPanel;
+    public GameObject fourthPanel;
 
     // Moves forward (Planet -> Rain)
     public void SwitchToRainPanel()
@@ -17,5 +19,33 @@ public class PanelToggle : MonoBehaviour
     {
         secondPanel.SetActive(false);
         firstPanel.SetActive(true);
+    }
+
+    // Moves foward (Rain -> Food)
+    public void SwitchToFoodPanel()
+    {
+        secondPanel.SetActive(false);
+        thirdPanel.SetActive(true);
+    }
+
+    // Moves backward (Food -> Rain)
+    public void GoBackToRain()
+    {
+        thirdPanel.SetActive(false);
+        secondPanel.SetActive(true);
+    }
+
+    //Moves forward (Food -> Wildlife)
+    public void SwitchToWildlifePanel()
+    {
+        thirdPanel.SetActive(false);
+        fourthPanel.SetActive(true);
+    }
+
+    //Moves backward (Wildlife -> Food)
+    public void GoBackToFood()
+    {
+        fourthPanel.SetActive(false);
+        thirdPanel.SetActive(true);
     }
 }
