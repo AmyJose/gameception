20fe6211db4ac16675cede107bc172329f321599
AmyJose <helloamyjose@gmail.com>
2026-02-03 14:38:29 +0000

    adding angle joint method

--- gameceptionUnity/Assets/Scenes/PoseDetectionHUD/PoseLandmarkHUD.cs
+++ gameceptionUnity/Assets/Scenes/PoseDetectionHUD/PoseLandmarkHUD.cs
@@ -1,4 +1,6 @@
+using Mediapipe.Tasks.Components.Containers;
 using Mediapipe.Tasks.Vision.PoseLandmarker;
+using System;
 using System.Text;
 using TMPro;
 using UnityEngine;
@@ -64,6 +66,7 @@ public class PoseLandmarkHUD : MonoBehaviour
 
         sb.Clear();
         sb.AppendLine($"Landmarks Detected: {lms.Count}");
+        sb.AppendLine($"Angle between 12 14 16: {getJointAngle(lms[12], lms[14], lms[16]):F2}");
 
         foreach(var idx in landmarkIndices)
         {
@@ -73,5 +76,26 @@ public class PoseLandmarkHUD : MonoBehaviour
         }
 
         return sb.ToString();
+    }
+
+    //getting angle at a joint
+    //code from https://developers.google.com/ml-kit/vision/pose-detection/classifying-poses#java
+    private double getJointAngle(NormalizedLandmark firstPoint, NormalizedLandmark midPoint, NormalizedLandmark lastPoint)
+    {
+        // computing the angle at midPoint formed by two segments
+        double result = 
+            (Math.Atan2(lastPoint.y - midPoint.y, lastPoint.x - midPoint.x) //last to mid vector
+            - Math.Atan2(firstPoint.y - midPoint.y, firstPoint.x - midPoint.x)) // first to mid vector
+            * (180.0 / Math.PI); //convert to degrees
+
+        //angle should never be negative
+        result = Math.Abs(result);
+
+        if (result > 180)
+        {
+            //get acute representation
+            result = (360.0 - result);
+        }
+        return result;
     }
 }
