5ec52466675653e8bedadb9136c8e3b8ee4f4f9d
AmyJose <helloamyjose@gmail.com>
2026-04-26 17:35:30 +0100

    making streak shake

--- gameceptionUnity/Assets/Scripts/UI/StreakUI.cs
+++ gameceptionUnity/Assets/Scripts/UI/StreakUI.cs
@@ -1,12 +1,35 @@
 using TMPro;
 using UnityEngine;
 using Gameplay;
+using System.Collections;
 
 public class StreakUI : MonoBehaviour
 {
     [SerializeField] private ScoreManager scoreManager;
     [SerializeField] private TMP_Text streakText;
-    [SerializeField] private int showFromStreak = 2;
+    [SerializeField] private int showFromStreak = 3;
+
+    [SerializeField] private float popScale = 1.35f;
+    [SerializeField] private float popDuration = 0.12f;
+    [SerializeField] private float settleDuration = 0.18f;
+    [SerializeField] private float shakeAmount = 8f;
+    [SerializeField] private float shakeDuration = 0.15f;
+
+    private int lastStreak;
+    private Vector3 baseScale;
+    private Vector2 baseAnchoredPosition;
+    private RectTransform rectTransform;
+    private Coroutine animRoutine;
+
+    private void Awake()
+    {
+        if (streakText != null)
+        {
+            rectTransform = streakText.rectTransform;
+            baseScale = rectTransform.localScale;
+            baseAnchoredPosition = rectTransform.anchoredPosition;
+        }
+    }
 
     private void OnEnable()
     {
@@ -29,14 +52,63 @@ public class StreakUI : MonoBehaviour
     {
         if (streakText == null) return;
 
-        if (streak >= showFromStreak)
+        bool shouldShow = streak >= showFromStreak;
+        streakText.gameObject.SetActive(shouldShow);
+
+        if (!shouldShow)
         {
-            streakText.gameObject.SetActive(true);
-            streakText.text = $"{streak}";
+            lastStreak = streak;
+            return;
         }
-        else
+
+        streakText.text = $"{streak}";
+
+        if (streak > lastStreak)
         {
-            streakText.gameObject.SetActive(false);
+            PlayPunch();
         }
+
+        lastStreak = streak;
+    }
+    private void PlayPunch()
+    {
+        if (animRoutine != null)
+            StopCoroutine(animRoutine);
+
+        animRoutine = StartCoroutine(PunchRoutine());
+    }
+
+    private IEnumerator PunchRoutine()
+    {
+        float timer = 0f;
+
+        while (timer < popDuration)
+        {
+            timer += Time.deltaTime;
+            float t = timer / popDuration;
+
+            rectTransform.localScale = Vector3.Lerp(baseScale, baseScale * popScale, t);
+            rectTransform.anchoredPosition = baseAnchoredPosition + (Random.insideUnitCircle * shakeAmount);
+
+            yield return null;
+        }
+
+        timer = 0f;
+
+        while (timer < settleDuration)
+        {
+            timer += Time.deltaTime;
+            float t = timer / settleDuration;
+            t = 1f - Mathf.Pow(1f - t, 3f);
+
+            rectTransform.localScale = Vector3.Lerp(baseScale * popScale, baseScale, t);
+            rectTransform.anchoredPosition = Vector3.Lerp(rectTransform.anchoredPosition, baseAnchoredPosition, t);
+
+            yield return null;
+        }
+
+        rectTransform.localScale = baseScale;
+        rectTransform.anchoredPosition = baseAnchoredPosition;
+        animRoutine = null;
     }
 }
